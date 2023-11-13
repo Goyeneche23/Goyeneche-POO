3f85f493f796cbd24eb9bb19e1e0762ba83b1ebf
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-11-13 17:46:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,60 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
-class ecuacionLineal {
-  private:
-      double a, b;
-  public:
-      ecuacionLineal(double a, double b) : 
-      void solucion() const {
-          if (a == 0) {
-              if (b == 0) {
-                  cout << "soluciones infinitas." << endl;
-              } 
-              else {
-                  cout << "no hay solución." << endl;
-              }
-          } 
-          else {
-              double solucion = -b / a;
-              cout << "Solución: x = " << solucion << endl;
-          }
-      }
-  };
+class EcuacionLineal {
+public:
+    EcuacionLineal(double a, double b) : a(a), b(b) {}
+    void resolver() const {
+        if (a == 0) {
+            if (b == 0) {
+                cout << "Infinitas soluciones." << endl;
+            } else {
+                cout << "No hay solución." << endl;
+            }
+        } else {
+            double solucion = -b / a;
+            cout << "x = " << solucion << endl;
+        }
+    }
 
-class ecuacionCuadratica {
-  public:
-      ecuacionCuadratica(double a, double b, double c) : 
-  };
+private:
+    double a, b;
+};
 
+class EcuacionCuadratica {
+public:
+    EcuacionCuadratica(double a, double b, double c) : a(a), b(b), c(c) {}
+    void resolver() const {
+
+        if ( (b * b - 4 * a * c ) > 0) {
+            double solucion1 = (-b + sqrt( b * b - 4 * a * c)) / (2 * a);
+            double solucion2 = (-b - sqrt( b * b - 4 * a * c)) / (2 * a);
+            cout << "x1 = " << solucion1 << endl << "x2 = " << solucion2 << endl;
+        } else if ( (b * b - 4 * a * c) == 0) {
+            double solucion = -b / (2 * a);
+            cout << "x = " << solucion << endl;
+        } else {
+            cout << "No solucion." << endl;
+        }
+    }
+
+private:
+    double a, b, c;
+};
+/*
+class ecuacionCubica {
+    public:
+    ecuacionCubica(double a, double b, double c, double d):
+    void solcucion()const {
+
+    }
+
+}
+*/
 int main() {
     int opcion;
-    double a, b;
+    double a, b, c;
 
     cout << "Seleccione el tipo de ecuación a resolver:" << endl;
     cout << "1. Ecuación Lineal" << endl;
@@ -45,8 +68,21 @@ int main() {
         cout << "Ingrese el coeficiente 'b' de la ecuación lineal: ";
         cin >> b;
 
-        ecuacionLineal ecuacionLineal(a, b);
-        ecuacionLineal.solucion();
+        EcuacionLineal ecuacionLineal(a, b);
+        ecuacionLineal.resolver();
+    } else if (opcion == 2) {
+        cout << "Ingrese el coeficiente 'a' de la ecuación cuadrática: ";
+        cin >> a;
+        cout << "Ingrese el coeficiente 'b' de la ecuación cuadrática: ";
+        cin >> b;
+        cout << "Ingrese el coeficiente 'c' de la ecuación cuadrática: ";
+        cin >> c;
+
+        EcuacionCuadratica ecuacionCuadratica(a, b, c);
+        ecuacionCuadratica.resolver();
+    } else {
+        cout << "Opción no válida." << endl;
     }
-    if else(opcion == 2) { 
+
+    return 0;
 }
