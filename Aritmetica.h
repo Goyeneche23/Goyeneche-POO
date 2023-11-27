90913ee3020dcd6df6a974eb3a4bdd54530723e0
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-11-27 18:12:23 -0500

    Update Aritmetica.h

--- Aritmetica.h
+++ Aritmetica.h
@@ -2,31 +2,62 @@
 #include <cmath>
 using namespace std;
 
-class Aritmetica{
-public:
-    Aritmetica(double a, double b) : a(a), b(b) {}
-    
-    void suma() const {
-        double solucion;
-        solucion = a + b;
-        cout << " solucion: " << solucion << endl;
-    }
-    void resta() const {
-        double solucion;
-        solucion = a - b;
-        cout << " solucion: " << solucion << endl;
-    }
-    void multiplicacion() const { 
-        double solucion;
-        solucion = a * b;
-        cout << " solucion: " << solucion << endl;
-    }
-    void division() const {
-        double solucion;
-        solucion = a / b;
-        cout << " solucion: " << solucion << endl;
-    }
-
-private: 
+// Clase madre OperacionMatematica
+class OperacionMatematica {
+protected:
     double a, b;
+
+public:
+    OperacionMatematica(double a, double b) : a(a), b(b) {}
+//operacionMatematica sera redefinida
+    virtual ~OperacionMatematica() {}
+
+    virtual void realizarOperacion() const = 0; 
+};
+
+// Clase hija Suma
+class Suma : public OperacionMatematica {
+public:
+    Suma(double a, double b) : OperacionMatematica(a, b) {}
+
+    void realizarOperacion() const override {
+        double solucion = a + b;
+        cout << "Suma: " << solucion << endl;
+    }
+};
+
+// Clase hija Resta
+class Resta : public OperacionMatematica {
+public:
+    Resta(double a, double b) : OperacionMatematica(a, b) {}
+
+    void realizarOperacion() const override {
+        double solucion = a - b;
+        cout << "Resta: " << solucion << endl;
+    }
+};
+
+// Clase hija Multiplicacion
+class Multiplicacion : public OperacionMatematica {
+public:
+    Multiplicacion(double a, double b) : OperacionMatematica(a, b) {}
+
+    void realizarOperacion() const override {
+        double solucion = a * b;
+        cout << "Multiplicacion: " << solucion << endl;
+    }
+};
+
+class Division : public OperacionMatematica {
+public:
+    Division(double a, double b) : OperacionMatematica(a, b) {}
+
+    void realizarOperacion() const override {
+        if (b != 0) {
+            double solucion = a / b;
+            cout << "Division: " << solucion << endl;
+        } else {
+            cout << "No se puede dividir entre cero." << endl;
+        }
+    }
 };
