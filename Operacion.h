f36ea00a972984db71ccb9703884997661796d53
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-11-27 19:36:34 -0500

    Update Operacion.h

--- Operacion.h
+++ Operacion.h
@@ -1,12 +1,17 @@
-// Clase madre OperacionMatematica
+
+#ifndef OPERACION_H
+#define OPERACION_H
+
+//clase Madre
 class OperacionMatematica {
 protected:
     double a, b;
 
 public:
     OperacionMatematica(double a, double b) : a(a), b(b) {}
-//operacionMatematica sera redefinida
     virtual ~OperacionMatematica() {}
 
-    virtual void realizarOperacion() const = 0; 
+    virtual void realizarOperacion() const = 0;
 };
+
+#endif
