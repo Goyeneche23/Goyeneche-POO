30743d161e912a9275ad9a6d179c1463da8e4003
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-11-27 19:37:29 -0500

    Update Aritmetica.h

--- Aritmetica.h
+++ Aritmetica.h
@@ -1,19 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include "Operacion.h"
 using namespace std;
-
-// Clase madre OperacionMatematica
-class OperacionMatematica {
-protected:
-    double a, b;
-
-public:
-    OperacionMatematica(double a, double b) : a(a), b(b) {}
-//operacionMatematica sera redefinida
-    virtual ~OperacionMatematica() {}
-
-    virtual void realizarOperacion() const = 0; 
-};
 
 // Clase hija Suma
 class Suma : public OperacionMatematica {
