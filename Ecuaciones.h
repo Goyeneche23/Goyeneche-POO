be29011dff7cca8febe6c61fc4d7ebe9555afb3e
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-11-27 19:37:04 -0500

    Update Ecuaciones.h

--- Ecuaciones.h
+++ Ecuaciones.h
@@ -3,12 +3,12 @@
 #include "Operacion.h"
 using namespace std;
 
-// Clase hija EcuacionLineal
+// EcuacionLineal
 class EcuacionLineal : public OperacionMatematica {
 public:
     EcuacionLineal(double a, double b) : OperacionMatematica(a, b) {}
 
-    void resolverEcuacion() const {
+    void realizarOperacion() const override {
         if (a == 0) {
             if (b == 0) {
                 cout << "Infinitas soluciones." << endl;
@@ -22,27 +22,28 @@ public:
     }
 };
 
-// Clase hija EcuacionCuadratica
+// EcuacionCuadratica
 class EcuacionCuadratica : public OperacionMatematica {
+    double c;
+
 public:
     EcuacionCuadratica(double a, double b, double c) : OperacionMatematica(a, b), c(c) {}
 
-    void resolverEcuacion() const {
-        if ((b * b - 4 * a * c) > 0) {
-            double solucion1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-            double solucion2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+    void realizarOperacion() const override {
+        double discriminante = b * b - 4 * a * c;
+        if (discriminante > 0) {
+            double solucion1 = (-b + sqrt(discriminante)) / (2 * a);
+            double solucion2 = (-b - sqrt(discriminante)) / (2 * a);
             cout << "x1 = " << solucion1 << endl << "x2 = " << solucion2 << endl;
-        } else if ((b * b - 4 * a * c) == 0) {
+        } else if (discriminante == 0) {
             double solucion = -b / (2 * a);
             cout << "x = " << solucion << endl;
         } else {
-            cout << "No solucion." << endl;
+            cout << "No hay solución real." << endl;
         }
     }
-
-private:
-    double c;
 };
+
 /*
 class EcuacionCubica {
 public:
